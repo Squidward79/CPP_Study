a4ee67b442e9c50670f255881f90882b43f60c3e
HominLee <ghalsgg99@naver.com>
2019-12-24 15:49:09 +0900

    챕터 1까지

--- Chapter1/mian_chapter1.cpp
+++ Chapter1/mian_chapter1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include "add.h" //내부 헤더는 "" 이다
+#include "add.h" 
 
-using namespace std; 
+using namespace std; //iostream 내부의 네임스페이스
 
 namespace my_funciton
 {
@@ -11,13 +11,23 @@ namespace my_funciton
 	}
 }
 
+#define MAX(a,b) ((a>b) ? a:b) //매크로 부분
+#define LIKE_APPLE
+
 int main()
 {
 	using namespace my_funciton;
+	myfunction(); //my_function::myfunction() 
 
-	myfunction();
+	//LIKE_APPLE가 정의되어 있는지 판단하여 다른 처리
+#ifdef LIKE_APPLE
+	cout << "apple" << endl;
+#else
+	cout << "orange" << endl;
+#endif // LIKE_APPLE
+
+
 
 	cout << add(1, 2) << endl;
-
 	return 0;
 }
